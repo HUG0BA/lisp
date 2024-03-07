920a462d77efe8b2490e3f2821243d7b38f9c578
HUG0BA <127461683+HUG0BA@users.noreply.github.com>
2024-03-07 10:50:31 -0600

    FileREader

--- src/main/java/uvg/App.java
+++ src/main/java/uvg/App.java
@@ -1,13 +1,24 @@
 package uvg;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws FileNotFoundException, IOException
     {
-        System.out.println( "Hello World!" );
+        FileManager fileManger = new FileManager();
+        ArrayList<String> test = fileManger.readTXTFile("example.txt");
+
+        for(String str : test){
+            System.out.println(str);
+        }
+
+
     }
 }
